6834438b145f8c0e14f827d21cc4c668b43b9a1e
PC-imatge-2022-2023 <licenses@enti.cat>
2023-04-17 19:07:42 +0200

    Search bar with chips (gmail)

--- app/src/main/java/com/example/classhangman/email/EmailActivity.kt
+++ app/src/main/java/com/example/classhangman/email/EmailActivity.kt
@@ -2,11 +2,44 @@ package com.example.classhangman.email
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.classhangman.R
+import com.example.classhangman.databinding.ActivityEmailBinding
+import com.google.android.material.chip.Chip
 
 class EmailActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEmailBinding
+
+    val possibleEmails = listOf(
+        "pau.garcia@enti.cat",
+        "pau.garcia@gmail.com",
+        "marta.garcia@gmail.cat",
+        "jose@enti.cat",
+        "montserrat@enti.cat"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_email)
+        binding = ActivityEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.emailToSerachBar.setAdapter(
+            ArrayAdapter(this, R.layout.item_search_view_email, possibleEmails)
+        )
+
+        binding.emailToSerachBar.setOnItemClickListener { parent, _, position, _ ->
+            val email = parent.getItemAtPosition(position).toString()
+            val chip = Chip(this)
+            chip.text = email
+            chip.setChipIconResource(R.drawable.ic_baseline_cancel_24)
+            chip.setOnClickListener {
+                binding.chipGroup.removeView(chip)
+            }
+
+            binding.chipGroup.addView(chip)
+            binding.emailToSerachBar.setText("")
+        }
+
     }
 }
