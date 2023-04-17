aae5fc634718aa039232fdd05407c52f2578fda0
PC-imatge-2022-2023 <licenses@enti.cat>
2023-04-17 20:43:33 +0200

    Deleted receiver declared on manifest

--- app/src/main/java/com/example/classhangman/email/BatteryReceiver.kt
+++ app/src/main/java/com/example/classhangman/email/BatteryReceiver.kt
@@ -1,11 +1,14 @@
 package com.example.classhangman.email
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
 class BatteryReceiver: BroadcastReceiver() {
+
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context?, intent: Intent?) {
         Toast.makeText(context, "Low battery from Manifest receiver", Toast.LENGTH_SHORT).show()
         println("Low battery from Manifest receiver")
